7812fd0a8e4e4892bb104542c6f689c4dd079fa3
zooyaho <jiwoo1268@naver.com>
2022-04-21 03:17:33 +0900

    ToDo List HTML&CSS UI 수정

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,25 +1,12 @@
-console.log('연결됨');
-
+/* flatpickr */
 let $dateSelector = document.querySelector('.dateSelector');
-
-// dateSelector.flatPicker({
-//   altInput: true,
-//   dateFormat: "YYYY-MM-DD",
-//   altFormat: "DD-MM-YYYY",
-//   allowInput: true,
-//   parseDate: (datestr, format) => {
-//     return moment(datestr, format, true).toDate();
-//   },
-//   formatDate: (date, format, locale) => {
-//     // locale can also be used
-//     return moment(date).format(format);
-//   }
-// });
 
 $dateSelector.flatpickr({
   dataFormat: "Y-m-d",
   minDate: "today",
-  maxDate: new Date().fp_incr(30) // 오늘 날짜 이전은 비활성, 오늘로부터 30일만 활성화하여 선택 가능한 옵션
-  
+  maxDate: new Date().fp_incr(30), // 오늘 날짜 이전은 비활성, 오늘로부터 30일만 활성화하여 선택 가능한 옵션
+  onChange: function(selectDates, dateStr, instance){
+  // 날짜가 선택되었을 때 수행할 일
+
+  },
 });
-console.log($dateSelector);
